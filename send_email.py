97d3b9bf106af8f513bc420f58756c0eca982242
syed <shezu30@gmail.com>
2023-10-30 02:36:46 +0530

    added

--- send_email.py
+++ send_email.py
@@ -10,8 +10,9 @@ def send_email(message):
 
 
      context = ssl.create_default_context()
+
      with smtplib.SMTP_SSL(host, port, context=context) as server:
-          server.login(username, password)
-          server.sendmail(username, receiver, message)
+              server.login(username, password)
+              server.sendmail(username, receiver, message)
 
 
